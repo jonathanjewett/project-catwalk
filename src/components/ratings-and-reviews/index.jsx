2460ed310fde7fd003ea7c96409cbd27db023592
Anthony Psyk <ajpsyk@gmail.com>
2021-10-27 11:37:57 -0700

    test change in ratings and reviews

--- src/components/ratings-and-reviews/index.jsx
+++ src/components/ratings-and-reviews/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// this is a test comment
+
 const RatingsAndReviews = () => (
   <div id="ratings-and-reviews">Ratings and Reviews</div>
 );
