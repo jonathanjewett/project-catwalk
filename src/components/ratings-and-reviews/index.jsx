28512d42d87ba8a794fd689a90d9814e64617090
Joshua Booth <joshua.n.booth@gmail.com>
2021-10-27 11:23:12 -0700

    Application framework

---
+++ src/components/ratings-and-reviews/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const RatingsAndReviews = () => (
+  <div id="ratings-and-reviews">Ratings and Reviews</div>
+);
+
+export default RatingsAndReviews;
