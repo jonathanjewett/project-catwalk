32bc3187c38af97311e20279a4e6164e88c5f0b0
Anthony Psyk <ajpsyk@gmail.com>
2021-10-28 14:55:15 -0700

    Resolved conflicts for package.json and App.test

---
+++ src/related-items-and-comparisons/App.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+it('displays all modules', () => {
+  const app = render(<App/>);
+
+  expect(document.querySelector('#overview'))
+    .toBeInTheDocument();
+
+  expect(document.querySelector('#questions-and-answers'))
+    .toBeInTheDocument();
+
+  expect(document.querySelector('#ratings-and-reviews'))
+    .toBeInTheDocument();
+
+  expect(document.querySelector('#related-items-and-comparisons'))
+    .toBeInTheDocument();
+});
