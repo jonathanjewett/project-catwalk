a018c5c976a84aae197e2464dbbd6617ad63da49
Joshua Booth <joshua.n.booth@gmail.com>
2021-10-29 13:11:28 -0700

    Display social media share buttons

--- src/overview/ProductInformation.jsx
+++ src/overview/ProductInformation.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  PinterestIcon,
+  PinterestShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+
+} from 'react-share';
 import { StarRating } from '../common';
 
 /** @param {string} priceString */
@@ -30,18 +39,38 @@ const Price = ({ base, sale }) =>
  * @param {number} props.rating
  * @param {Style} props.style
  */
-const ProductInformation = ({ product, rating, style }) => (
-  <div>
-    <p>
-      <StarRating rating={rating}/>
-      <a href="#ratings-and-review">Read all reviews</a>
-    </p>
-    <p className="category">{product.category}</p>
-    <h2>{product.name}</h2>
-    <h4>{product.slogan}</h4>
-    <p>{product.description}</p>
-    <Price base={style.original_price} sale={style.sale_price}/>
-  </div>
-);
+const ProductInformation = ({ product, rating, style }) => {
+  const url = `https://www.project-catwalk.com/${product.id}`;
+  return (
+    <div>
+      <p>
+        <StarRating rating={rating}/>
+        <a href="#ratings-and-review">Read all reviews</a>
+      </p>
+      <p className="category">{product.category}</p>
+      <h2>{product.name}</h2>
+      <h4>{product.slogan}</h4>
+      <p>{product.description}</p>
+      <Price base={style.original_price} sale={style.sale_price}/>
+      Share on:
+      <FacebookShareButton url={url}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={url}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+      <PinterestShareButton url={url}>
+        <PinterestIcon size={32} round />
+      </PinterestShareButton>
+    </div>
+  );
+};
+/*
 
+
+Facebook
+Twitter
+Pinterest
+
+*/
 export default ProductInformation;
