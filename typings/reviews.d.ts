76451cf53bde865b68f6bbf968ef88243d2d44a3
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-01 11:37:57 -0700

    More documentation of API response types

--- typings/reviews.d.ts
+++ typings/reviews.d.ts
@@ -1,3 +1,6 @@
+// `GET /products/:product_id/styles` returns an object whose `.results` property is an array of
+// `Review`s.
+// i.e. it returns `{results: [review1, review2, ...], ...}`
 interface Review {
     review_id: number,
     rating: number,
@@ -11,9 +14,10 @@ interface Review {
     photos: {id: number, url: string}[]
 }
 
+// `GET /reviews/meta` returns a `ReviewMetadata`.
 interface ReviewMetadata {
     product_id: string,
     ratings: {1: number, 2: number, 3: number, 4: number, 5: number}
-    recommended: {[recommendation: number]: number},
+    recommended: {true: number, false: number},
     characteristics: {[characteristic: string]: {id: number, value: string}}
 }
