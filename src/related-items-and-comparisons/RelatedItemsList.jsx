c903ad7416cc39cc2c25486cf72c59dfeb4879e8
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-03 18:10:59 -0500

    buttons added for now for MVP, figuring out overflow/how to keep right button on screen while excess cards are off screen

--- src/related-items-and-comparisons/RelatedItemsList.jsx
+++ src/related-items-and-comparisons/RelatedItemsList.jsx
@@ -4,7 +4,9 @@ import CardItem from './CardItem.jsx';
 const RelatedItemsList = (props) => {
   return (
     <div className="related-items-list">
+      <button type="button" className="left-arrow"> &lt; </button>
       {props.products.map((product) => <CardItem key={product.id} product={product} />)}
+      <button type="button" className="right-arrow"> &gt; </button>
     </div>
   );
 };
