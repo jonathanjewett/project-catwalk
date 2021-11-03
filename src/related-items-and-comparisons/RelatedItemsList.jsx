012ba34b6ac246b1d5b0681fd677ce4122d4256b
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-03 18:55:42 -0500

    buttons stay in the foreground and move relative to the shop front

--- src/related-items-and-comparisons/RelatedItemsList.jsx
+++ src/related-items-and-comparisons/RelatedItemsList.jsx
@@ -3,9 +3,11 @@ import CardItem from './CardItem.jsx';
 
 const RelatedItemsList = (props) => {
   return (
-    <div className="related-items-list">
+    <div className="related-container">
       <button type="button" className="left-arrow"> &lt; </button>
-      {props.products.map((product) => <CardItem key={product.id} product={product} />)}
+      <div className="related-items-list">
+        {props.products.map((product) => <CardItem key={product.id} product={product} />)}
+      </div>
       <button type="button" className="right-arrow"> &gt; </button>
     </div>
   );
