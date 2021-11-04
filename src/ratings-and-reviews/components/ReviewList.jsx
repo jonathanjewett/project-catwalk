92c2d9b326466973ce98fdcb29a9ad7784494a16
Anthony Psyk <ajpsyk@gmail.com>
2021-11-03 20:57:40 -0700

    Renders only 2 reviews by default, everytime 'more reviews' button is clicked, 2 more are rendered

--- src/ratings-and-reviews/components/ReviewList.jsx
+++ src/ratings-and-reviews/components/ReviewList.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReviewTile from './ReviewTile.jsx';
 
-const ReviewList = ({reviewList}) => (
 
-  <div className="review-list">
-    {reviewList.map((review) => <ReviewTile review={review} key={review.review_id}/>)}
-  </div>
-);
+
+const ReviewList = ({reviewList}) => {
+
+  let [count, setCount] = useState(2);
+
+  return (
+    <div className="review-list">
+      {reviewList.slice(0, count).map((review) => <ReviewTile review={review} key={review.review_id}/>)}
+      { count <= reviewList.length &&
+        <button id="more-reviews" onClick={() => setCount(count + 2) }>More Reviews</button>
+      }
+      <button className="add-a-review">Add A Review +</button>
+    </div>
+  );
+};
 
 export default ReviewList;
