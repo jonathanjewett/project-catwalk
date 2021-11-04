f2811586e2e715654a940fe828770f0b0f4f591f
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-04 18:04:31 -0500

    Removed extra variable, added comment clarifying what setCurrIndex was doing, and refactored buttons to render without violating DRY

--- src/related-items-and-comparisons/RelatedItemsList.jsx
+++ src/related-items-and-comparisons/RelatedItemsList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import CardItem from './CardItem.jsx';
 
 const RelatedItemsList = (props) => {
-  const allProducts = props.products;
   const products = props.products;
   const [currList, setCurrList] = useState(products);
   const [currIndex, setCurrIndex] = useState(0);
@@ -10,8 +9,8 @@ const RelatedItemsList = (props) => {
   // "useEffect is componentDidMount, componentDidUpdate, and componentWillUnmount combined"
 
   const next = () => {
-    setCurrIndex(currIndex + 1);
-    setCurrList(products.slice(currIndex + 1)); //bandaid fix, still not sure why its not updating the array without the +1 here
+    setCurrIndex(currIndex + 1); // this is changing what currIndex will be the next time the component renders
+    setCurrList(products.slice(currIndex + 1));
   };
 
   const prev = () => {
@@ -19,44 +18,25 @@ const RelatedItemsList = (props) => {
     setCurrList(products.slice(currIndex - 1));
   };
 
-  if (currIndex === products.length - 1) {
-    return (
-      <div>
-        <h3>RELATED PRODUCTS</h3>
-        <div className="related-container">
-          <button type="button" className="left-arrow" onClick={prev}> &lt; </button>
-          <div className="related-items-list">
-            {currList.map((product) => <CardItem key={product.id} product={product} />)}
-          </div>
+  const rightButton = currIndex === products.length - 1 ? null :
+    <button type="button" className="right-arrow" onClick={next}> &gt; </button>;
+
+  const leftButton = currIndex === 0 ? null :
+    <button type="button" className="left-arrow" onClick={prev}> &lt; </button>;
+
+
+  return (
+    <div>
+      <h3>RELATED PRODUCTS</h3>
+      <div className="related-container">
+        {leftButton}
+        <div className="related-items-list">
+          {currList.map((product) => <CardItem key={product.id} product={product} />)}
         </div>
+        {rightButton}
       </div>
-    );
-  } else if (currIndex === 0) {
-    return (
-      <div>
-        <h3>RELATED PRODUCTS</h3>
-        <div className="related-container">
-          <div className="related-items-list">
-            {currList.map((product) => <CardItem key={product.id} product={product} />)}
-          </div>
-          <button type="button" className="right-arrow" onClick={next}> &gt; </button>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h3>RELATED PRODUCTS</h3>
-        <div className="related-container">
-          <button type="button" className="left-arrow" onClick={prev}> &lt; </button>
-          <div className="related-items-list">
-            {currList.map((product) => <CardItem key={product.id} product={product} />)}
-          </div>
-          <button type="button" className="right-arrow" onClick={next}> &gt; </button>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default RelatedItemsList;
