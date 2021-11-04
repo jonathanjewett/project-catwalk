295c8773863022c3e7a4281ec14c70b6cdeb99e8
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-04 12:53:47 -0500

    right arrow button works but array doesnt change until second click and after 4th/5th click it just returns an empty array for some reason

--- src/related-items-and-comparisons/RelatedItemsList.jsx
+++ src/related-items-and-comparisons/RelatedItemsList.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CardItem from './CardItem.jsx';
 
 const RelatedItemsList = (props) => {
+  const allProducts = props.products;
+  var products = props.products;
+  const [currList, setCurrList] = useState(products);
+  const [currIndex, setCurrIndex] = useState(0);
+  const [length, setLength] = useState(products.length);
+
+  // set length to current products
+  // "useEffect is componentDidMount, componentDidUpdate, and componentWillUnmount combined"
+  useEffect(() => {
+
+  });
+
+  const next = () => {
+    setCurrIndex(prevState => prevState + 1);
+    setCurrList(prevState => prevState.slice(currIndex));
+  };
+
   return (
-    <div className="related-container">
-      <button type="button" className="left-arrow"> &lt; </button>
-      <div className="related-items-list">
-        {props.products.map((product) => <CardItem key={product.id} product={product} />)}
+    <div>
+      <h3>RELATED PRODUCTS</h3>
+      <div className="related-container">
+        <button type="button" className="left-arrow"> &lt; </button>
+        <div className="related-items-list">
+          {currList.map((product) => <CardItem key={product.id} product={product} />)}
+        </div>
+        <button type="button" className="right-arrow" onClick={next}> &gt; </button>
       </div>
-      <button type="button" className="right-arrow"> &gt; </button>
     </div>
   );
 };
