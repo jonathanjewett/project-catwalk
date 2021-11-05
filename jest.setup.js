e908fb54ef239d6bddfab61df8135fffbc5ee08f
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-05 11:36:45 -0700

    API testing framework

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,19 @@
 import '@testing-library/jest-dom';
-jest.mock('./src/api.js');
+import 'regenerator-runtime';
+import axios from 'axios';
+
+jest.mock('axios', () => ({
+  create: () => {
+    const mock = {};
+    for (const method of ['GET', 'POST', 'PUT']) {
+      mock[method.toLowerCase()] = (...args) => {
+        const log = process.mockRequestLog;
+        if (log) {
+          args.unshift(method);
+          log.push(args);
+        }
+      };
+    }
+    return mock;
+  }
+}));
