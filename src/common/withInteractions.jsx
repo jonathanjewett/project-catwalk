24bdc47dcbab1e3d892c25daeb9c56ffa541301a
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-05 13:25:23 -0700

    Remove superfluous file

--- src/common/withInteractions.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import api from '../api';
-
-const withInteractions = (Component, props) =>
-  <Component onClick={(/** @type {React.MouseEvent} */ event) => api.logInteraction(
-    event.target
-  )}/>;
