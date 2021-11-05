5393fbdd31635f1e42643b32959a7022a6d9374d
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-05 13:34:33 -0500

    basic modal added, working on button functionality now

---
+++ src/related-items-and-comparisons/ComparisonModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComparisonModal = (props) => {
+  return (
+    <div className="comparison-modal">
+      Placeholder for making modal window.
+    </div>
+  );
+};
+
+export default ComparisonModal;
--- src/related-items-and-comparisons/index.jsx
+++ src/related-items-and-comparisons/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardItem from './CardItem.jsx';
 import RelatedItemsList from './RelatedItemsList.jsx';
+import ComparisonModal from './ComparisonModal.jsx';
 import './related-items-and-comparisons.scss';
 
 import { products, stylesForProducts, relatedProducts } from './sampleData.js';
@@ -9,6 +10,7 @@ const RelatedItemsAndComparisons = (props) => (
   <div id="related-items-and-comparisons">
     <div>
       <RelatedItemsList products={products}/>
+      <ComparisonModal/>
     </div>
   </div>
 );
