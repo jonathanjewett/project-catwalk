c4636d716e1887449ee8e9b37077f3dba32ecec0
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-05 19:00:03 -0500

    created table with placeholders. Need to make them dynamically now

--- src/related-items-and-comparisons/ComparisonModal.jsx
+++ src/related-items-and-comparisons/ComparisonModal.jsx
@@ -3,8 +3,28 @@ import React from 'react';
 const ComparisonModal = (props) => {
   return (
     <div className="comparison-modal">
-      Placeholder for making modal window.
       <button type="button" onClick={props.close}>X</button>
+      <table className="comparison-table">
+        <thead>
+          <tr>
+            <th>product 1</th>
+            <th></th>
+            <th>product 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>X</td>
+            <td>Too Expensive</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Zombie Proof</td>
+            <td>X</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
