573491a030d983c90b2dc9b1f7dad20224e1c509
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-06 16:36:29 -0700

    Mark placeholder as optional

--- src/common/list-view/Search.jsx
+++ src/common/list-view/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 /**
  * @param {Object} props
- * @param {string} props.placeholder
+ * @param {string=} props.placeholder
  * @param {string} props.value
  * @param {React.Dispatch<React.SetStateAction<string>>} props.set
  */
