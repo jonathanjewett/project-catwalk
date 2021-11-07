6cae45fae00cd3e5f00311d45630b3de086ce343
Anthony Psyk <ajpsyk@gmail.com>
2021-11-07 12:38:35 -0800

    Moved state hook to parent component

--- src/ratings-and-reviews/components/ReviewList.jsx
+++ src/ratings-and-reviews/components/ReviewList.jsx
@@ -3,17 +3,11 @@ import ReviewTile from './ReviewTile.jsx';
 
 
 
-const ReviewList = ({reviews}) => {
-
-  let [count, setCount] = useState(2);
+const ReviewList = ({count, reviews, filteredReviews}) => {
 
   return (
     <div className="review-list">
       {reviews.slice(0, count).map((review) => <ReviewTile review={review} key={review.review_id}/>)}
-      { count <= reviews.length &&
-        <button id="more-reviews" onClick={() => setCount(count + 2) }>More Reviews</button>
-      }
-      <button className="add-a-review">Add A Review +</button>
     </div>
   );
 };
