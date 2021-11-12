b90ded18f084d486746ccfc8f7bc8e2ba294ac94
Anthony Psyk <ajpsyk@gmail.com>
2021-11-11 18:40:40 -0800

    Resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ import RelatedItemsAndComparisons from './related-items-and-comparisons';
 const App = ({ info, questions, related, reviews }) => {
   return (
     <div>
-      <Overview info={info} reviewCount={reviews.length}/>
-      <RelatedItemsAndComparisons products={related}/>
-      <QuestionsAndAnswers questions={questions} product={info.product}/>
-      <RatingsAndReviews product={info.product} reviews={reviews} metadata={info.metadata}/>
+      <Tracker render={Overview} info={info} reviewCount={reviews.length}/>
+      <Tracker render={RelatedItemsAndComparisons} products={related} info={info}/>
+      <Tracker render={QuestionsAndAnswers} questions={questions} product={info.product}/>
+      <Tracker render={RatingsAndReviews} product={info.product} reviews={reviews} metadata={info.metadata}/>
     </div>
   );
 };
