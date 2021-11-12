f26263dd52a369e381248b8c3a6f77f9cdb87192
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-12 14:57:49 -0800

    Add header component

---
+++ src/header/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Favicon from '../favicon.svg';
+import './Header.scss';
+import { Price } from '../common';
+
+/**
+ * @param {Object} props
+ * @param {string=} props.announcement
+ * @param {{id: number, name: string, base: string, sale: string}} props.discount
+ * @param {{id: number, message: string}} props.newProduct
+ */
+const Header = ({ announcement, discount, newProduct }) => {
+  const elements = [];
+  if (announcement) {
+    elements.push(<em key="announcement">{announcement}</em>);
+    elements.push(' — ');
+  }
+  if (discount) {
+    elements.push(
+      <span key="discount">
+        <a href={`/${discount}`}>{discount.name}</a>:
+        <Price base={discount.base} sale={discount.sale}/>
+      </span>
+    );
+    elements.push(' — ');
+  }
+  if (newProduct) {
+    elements.push(
+      <a href={`/${newProduct.id}`} key="new">
+        {newProduct.message}
+      </a>
+    );
+    elements.push(' — ');
+  }
+  elements.pop();
+  return (
+    <header>
+      <div className="branding">
+        <Favicon/>
+        <h1>Project Catwalk</h1>
+      </div>
+      <div className="highlights">
+        {elements}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
