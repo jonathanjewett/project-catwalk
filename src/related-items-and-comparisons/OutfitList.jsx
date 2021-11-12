f3fd1cae5a2631ef5b8a46e49ebd843520439a3b
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-11 18:13:07 -0600

    removed unnecessary comments from outfitlist

--- src/related-items-and-comparisons/OutfitList.jsx
+++ src/related-items-and-comparisons/OutfitList.jsx
@@ -2,22 +2,9 @@ import React, { useState } from 'react';
 import OutfitCard from './OutfitCard.jsx';
 
 const OutfitList = (props) => {
-  // hook for array of outfits, starts with nothing by default
-  // items must be unique, so clicking does nothing if product is already in the list
-  // should always have an "add to list" card
-  // var storedFits = JSON.parse(localStorage.getItem('outfit')) || [];
-  // var storedIDs = JSON.parse(localStorage.getItem('outfitIDs')) || [];
   const [outfit, updateOutfit] = useState(() => JSON.parse(localStorage.getItem('outfit')) || []);
   const [outfitIDs, updateOutfitIDs] = useState(() => JSON.parse(localStorage.getItem('outfitIDs')) || []);
-  // const [outfitIDs, updateOutfitIDs] = useState(storedIDs);
-  // const [outfit, updateOutfit] = useState(storedFits);
-  // var outfitIDs = storedIDs;
-  // var outfit = storedFits;
   const [currIndex, setCurrIndex] = useState(0);
-
-  // using this for a homemade forceUpdate
-  // const [val, setVal] = useState(0);
-
 
   const next = () => {
     setCurrIndex(currIndex + 1); // this is changing what currIndex will be the next time the component renders
@@ -29,11 +16,6 @@ const OutfitList = (props) => {
 
   const addToOutfit = () => {
     if (!outfitIDs.includes(props.currentProduct.product.id)) {
-      // updateOutfit([...outfit, props.currentProduct]);
-      // updateOutfitIDs([...outfitIDs, props.currentProduct.product.id]);
-      // outfit.push(props.currentProduct);
-      // outfitIDs.push(props.currentProduct.product.id);
-      // setVal(val => val + 1);
       const newOutfit = [...outfit, props.currentProduct];
       localStorage.setItem('outfit', JSON.stringify(newOutfit));
       updateOutfit(newOutfit);
@@ -41,8 +23,6 @@ const OutfitList = (props) => {
       localStorage.setItem('outfitIDs', JSON.stringify(newOutfitIDs));
       updateOutfitIDs(newOutfitIDs);
     }
-    // localStorage.setItem('outfit', JSON.stringify(outfit));
-    // localStorage.setItem('outfitIDs', JSON.stringify(outfitIDs));
   };
 
   const removeFromOutfit = (product) => {
@@ -59,32 +39,6 @@ const OutfitList = (props) => {
     updateOutfit(newOutfitRemoved);
     localStorage.setItem('outfitIDs', JSON.stringify(newOutfitIDsRemoved));
     updateOutfitIDs(newOutfitIDsRemoved);
-
-    /*
-    var index = -1;
-    for (var i = 0; i < outfit.length; i++) {
-      if (outfit[i].product.id === product.id) {
-        index = i;
-      }
-    }
-    if (index > -1) {
-      console.log(index);
-      var newOutfitRemove = outfit.splice(index, 1);
-      console.log(newOutfitRemove);
-      localStorage.setItem('outfit', JSON.stringify(newOutfitRemove));
-      updateOutfit(newOutfitRemove);
-      const newOutfitIDs = outfitIDs.splice(index, 1);
-      localStorage.setItem('outfitIDs', JSON.stringify(newOutfitIDs));
-      updateOutfitIDs(newOutfitIDs);
-      // updateOutfit(prevState => outfit.splice(index, 1));
-      // updateOutfitIDs(prevState => outfitIDs.splice(index, 1));
-      // outfit.splice(index, 1);
-      // outfitIDs.splice(index, 1);
-      // setVal(val => val + 1);
-    }
-    // localStorage.setItem('outfit', JSON.stringify(outfit));
-    // localStorage.setItem('outfitIDs', JSON.stringify(outfitIDs));
-    */
   };
 
 
