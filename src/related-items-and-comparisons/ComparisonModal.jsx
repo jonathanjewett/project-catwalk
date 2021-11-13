5ea674ea8aa74e78869da827f94ffd9bcea2a613
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-12 20:27:07 -0800

    Simply feature comparison

--- src/related-items-and-comparisons/ComparisonModal.jsx
+++ src/related-items-and-comparisons/ComparisonModal.jsx
@@ -4,48 +4,25 @@ const ComparisonModal = (props) => {
   // refactor later after full implementation so it doesn't need this function
   const renderTableData = () => {
     // Make an array of all features between the two products
-    var features = [];
-    props.product1.features.map((feature) => features.push(feature.feature));
-    for (var i = 0; i < props.product2.features.length; i++) {
-      if (!features.includes(props.product2.features[i].feature)) {
-        features.push(props.product2.features[i].feature);
-      }
+    const features = {};
+    for (const { feature, value } of props.product1.features) {
+      features[feature] = { value1: value, value2: null };
     }
-
-    // Create object array with the features and values for product1
-    var featuresAndValues = [];
-    for (var i = 0; i < features.length; i++) {
-      // if product1 has feature, value1 is set, if not, value1 is null
-      var valueAdded = false;
-      for (var j = 0; j < props.product1.features.length; j++) {
-        if (props.product1.features[j].feature === features[i]) {
-          featuresAndValues.push({feature: features[i], value1: props.product1.features[j].value, value2: null});
-          valueAdded = true;
-        }
-      }
-
-      // if the feature wasnt found in product1, value1 will be null
-      if (valueAdded === false) {
-        featuresAndValues.push({feature: features[i], value1: null, value2: null});
-      }
-    }
-
-    // Iterate through featuresAndValues to add product2 values
-    for (var i = 0; i < featuresAndValues.length; i++) {
-      for (var j = 0; j < props.product2.features.length; j++) {
-        if (props.product2.features[j].feature === featuresAndValues[i].feature) {
-          featuresAndValues[i].value2 = props.product2.features[j].value;
-        }
+    for (const { feature, value } of props.product2.features) {
+      if (feature in features) {
+        features[feature].value2 = value;
+      } else {
+        features[feature] = { value1: null, value2: value };
       }
     }
 
     // return table row
-    return featuresAndValues.map((feature) => {
+    return Object.entries(features).map(([feature, { value1, value2 }]) => {
       return (
-        <tr key={feature.feature}>
-          <td>{feature.value1}</td>
-          <td>{feature.feature}</td>
-          <td>{feature.value2}</td>
+        <tr key={feature}>
+          <td>{value1}</td>
+          <td>{feature}</td>
+          <td>{value2}</td>
         </tr>
       );
     });
